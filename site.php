2d6c7d5e555ede9fa73b90da0422cecfd574b8a8
Dominik Haeusser <haeusser-dominik@web.de>
2021-04-12 19:23:49 +0200

    init commit

---
+++ site.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+        echo "<h1>Domroon's Site</h1>" ;
+        echo "<hr>";
+        echo "<p>This is my site</p>";
+    ?>
+
+    <article>
+    <?php
+        $characterName = "Tom";
+        $characterAge = 42;
+        echo "There once was a man named $characterName <br>";
+        echo "Hew as $characterAge years old <br>";
+        echo "He really liked the name $characterName <br>";
+        echo "But he didn't like being $characterAge <br>";
+    ?>
+    </article>
+</body>
+</html>
