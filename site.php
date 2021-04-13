54c8c04a2e31447b35fefe5e606b884b8d5bf549
Dominik Haeusser <haeusser-dominik@web.de>
2021-04-13 12:31:32 +0200

    add 'Object Functions'

--- site.php
+++ site.php
@@ -411,5 +411,37 @@
         ?>       
     </article>
 
+    <article>
+    <h2>Object Functions</h2> 
+        <?php
+            class Student {
+                var $name;
+                var $major;
+                var $gpa;
+
+                function __construct($name, $major, $gpa){
+                    $this->name = $name;
+                    $this->major = $major;
+                    $this->gpa = $gpa;
+                }
+
+                function hasHonors(){
+                    if($this->gpa >= 3.5){
+                        return "true <br>";
+                    } else {
+                        return "false <br>";
+                    }
+                }
+            }
+            
+
+            $student1 = new Student("Jim", "Business", 2.8);
+            $student2 = new Student("Pam", "Art", 3.6);
+
+            echo $student1->hasHonors();
+            echo $student2->hasHonors();
+        ?>       
+    </article>
+
 </body>
 </html>
